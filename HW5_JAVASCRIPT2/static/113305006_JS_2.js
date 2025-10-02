2451bbd0ee974f1b6ed66974edbb77c6ebaa2422
Rayyang800 <ray20040424@gmail.com>
2025-10-02 14:34:44 +0800

    js2CalFunction

---
+++ HW5_JAVASCRIPT2/static/113305006_JS_2.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const checkboxAll = document.getElementById("checkbox_all");
+  const checkboxes = document.querySelectorAll(".item-checkbox");
+  const checkoutBtn = document.getElementById("checkout");
+  const totalSpan = document.getElementById("total");
+  const checkoutResult = document.getElementById("checkout-result");
+    // 計算總金額
+  function calculateTotal() {
+    let total = 0;
+    document.querySelectorAll("tbody tr").forEach(row => {
+      const checkbox = row.querySelector(".item-checkbox");
+      const subtotal = parseInt(row.querySelector(".subtotal").textContent);
+      if (checkbox.checked) total += subtotal;
+    });
+    totalSpan.textContent = total;
+  }
